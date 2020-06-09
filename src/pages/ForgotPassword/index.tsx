1372ae3891cfbb2c18cb7ac6cca316be94af7c35
Vitor <vop1234@hotmail.com>
2020-06-09 06:09:16 -0300

    Creating recovery page

--- src/pages/ForgotPassword/index.tsx
+++ src/pages/ForgotPassword/index.tsx
@@ -1,11 +1,10 @@
 import React, { useRef, useCallback } from 'react'
-import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
+import { FiLogIn, FiMail } from "react-icons/fi";
 import { FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
 import * as Yup from 'yup'
-import { Link, useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-import { useAuth } from '../../hooks/auth'
 import { useToast } from '../../hooks/toast'
 import getValidadtionsErrors from '../../utils/getValidationErrors'
 
@@ -16,21 +15,17 @@ import Button from '../../components/Button'
 
 import { Container, Content, AnimationContainer, Background } from "./styles";
 
-interface SingInFormData {
+interface ForgotPasswordFormData {
   email: string,
-  password: string
 }
 
-const SingnIn: React.FC = () => {
+const ForgotPassword: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
 
-  const { singIn } = useAuth()
   const { addToast } = useToast()
 
-  const history = useHistory()
-
   const handleSubmit = useCallback(
-    async (data: SingInFormData) => {
+    async (data: ForgotPasswordFormData) => {
       try {
         formRef.current?.setErrors({})
 
@@ -38,19 +33,12 @@ const SingnIn: React.FC = () => {
           email: Yup.string()
             .required('E-mail obrigatorio.')
             .email('Digite um e-mail valido.'),
-          password: Yup.string().required('Senha obrigatória.')
         })
 
         await schema.validate(data, {
           abortEarly: false
         })
 
-        await singIn({
-          email: data.email,
-          password: data.password
-        })
-
-        history.push('/dashboard')
 
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
@@ -63,11 +51,11 @@ const SingnIn: React.FC = () => {
         // reacti18n para fazer internacionalização...
         addToast({
           type: 'error',
-          title: 'Erro na autenticação',
-          description: 'Ocorreu um erro ao fazer login, cheque as credenciais.',
+          title: 'Erro na recuperação de senha',
+          description: 'Ocorreu um erro ao tentar realizar a recuperação de senha, tente noavamete.',
         })
       }
-    }, [singIn, addToast, history])
+    }, [addToast])
 
   return (
     <Container>
@@ -75,17 +63,15 @@ const SingnIn: React.FC = () => {
         <AnimationContainer>
           <img src={logoImg} alt="GoBarber" />
           <Form ref={formRef} onSubmit={handleSubmit}>
-            <h1>Faça seu Logon</h1>
+            <h1>Recuperar Senha</h1>
             <Input name="email" type="text" icon={FiMail} placeholder="E-mail" />
-            <Input name="password" type="password" icon={FiLock} placeholder="Senha" />
 
-            <Button type="submit">Entrar</Button>
+            <Button type="submit">Recuperar</Button>
 
-            <Link to="/forgot-password">Esqueci minha Senha</Link>
           </Form>
-          <Link to="/signup">
+          <Link to="/">
             <FiLogIn />
-          Criar conta
+          Voltar
         </Link>
         </AnimationContainer>
       </Content>
@@ -95,4 +81,4 @@ const SingnIn: React.FC = () => {
 }
 
 
-export default SingnIn
+export default ForgotPassword
