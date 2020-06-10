3a7a0df30d90908da6a977450c6f7bf49bf2df90
Vitor <vop1234@hotmail.com>
2020-06-10 06:57:21 -0300

    Creating Header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,42 @@
 import React from 'react'
 
+import { Container, Header, HeaderContent, Profile } from './styles'
+
+import logo from '../../assets/logo.svg'
+import { FiPower } from 'react-icons/fi'
+import { useAuth } from '../../hooks/auth'
+
 const Dashboard: React.FC = () => {
-  return (<h1>Dashboard</h1>)
+  const { signOut, user } = useAuth();
+
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logo} alt="GoBarber" />
+
+          <Profile>
+            <img
+              src={user.avatar_url}
+              alt={user.name}
+            />
+            <div>
+              <span>Bem-Vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  )
 }
+
+
+
 
 export default Dashboard
